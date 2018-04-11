f4613b95498469db686a947d5b64ee48c00f9e83
Jason Kelley <jasonkelley@Jasons-MacBook-Pro-3.local>
2018-04-11 18:34:23 -0500

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+var animation = lottie.loadAnimation({
+  container: document.getElementById('animation'), // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'data.json' // the path to the animation json
+});
